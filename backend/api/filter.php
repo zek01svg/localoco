c486809daf7b02805d24a5e0ed504d4ada8aaef5
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-10-14 00:53:46 +0800

    added extra error reporting functions; modified paths; added an else block for the db calling method to save db calls

--- backend/api/filter.php
+++ backend/api/filter.php
@@ -1,18 +1,24 @@
 <?php
-require_once '../utils/common.php';
-require_once '../utils/functions.php';
+ob_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../utils/common.php';
+require_once __DIR__ . '/../utils/functions.php';
 
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     
-    $businesses = $businessDAO->getAllBusinesses();
-
     // if there are filters
     if (isset($_GET['filters'])) {
         $filters = json_decode($_GET['filters'], true);
         $businesses = $businessDAO->FilterAndSearch($filters);
+    }
+    else {
+        $businesses = $businessDAO->getAllBusinesses();
     }
 
     echo json_encode($businesses);
